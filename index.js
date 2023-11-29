23f11459d8a942232dea5f92b1a0fe3d925b94a8
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2023-11-29 16:35:36 +0200

    Add files via upload
    show numbers on screen

--- index.js
+++ index.js
@@ -1,48 +1,156 @@
 
-
 var num1 = 0;
 var num2 = 0;
+var arr = [];
 
-if (document.querySelector(".number")){ 
-    document.querySelector(".number").addEventListener("click", function () {
-    return num1 = document.querySelector(".number")});
-}    
+//  <-- select first number --> 
 
-
-
-// document.querySelector(".number").addEventListener("click", function () {
-//     return num1 = document.querySelector(".number");
-// });
-// document.querySelector(".number").addEventListener("click", function () {
-//     return num2 = document.querySelector(".number");
-// });
-
-// if (document.querySelector("#equal").addEventListener("click", calculate)) {
-//     document.querySelector("h3").classList.toggle("hidden");
-// }
-
-// <-- math symbols when clicked -->
-document.querySelector("#plus").addEventListener("click", 
-    function add (num1, num2) {
-        return num1 + num2;
+$("button").on("click", (event) => {
+    var a = event.target.id;
+    switch(a){
+        case "numberone":
+            num1 = $("#numberone").text();
+            $(".firstNumber").html(num1);
+            secondNum();
+            break;
+        case "numbertwo":
+            num1 = $("#numbertwo").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numberthree":
+            num1 = $("#numberthree").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numberfour":
+            num1 = $("#numberfour").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numberfive":
+            num1 = $("#numberfive").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numbersix":
+            num1 = $("#numbersix").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numberseven":
+            num1 = $("#numberseven").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numbereight":
+            num1 = $("#numbereight").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numbernine":
+            num1 = $("#numbernine").text();
+            $(".firstNumber").html(num1);
+            break;
+        case "numberzero":
+            num1 = $("#numberzero").text();
+            $(".firstNumber").html(num1);
+            break;
+    }
 });
 
-document.querySelector("#minus").addEventListener("click", 
-    function sub (num1, num2) {
-        return num1 - num2;
+//  <-- select operator -->
+
+$("button").on("click", (event) => {
+    var name = event.target.id;
+    switch(name) {
+        case "plus":
+            $(".calculation").html("+");
+            break;
+        case "minus":
+            $(".calculation").html("-");
+            break;
+        case "times":
+            $(".calculation").html("*");
+            break;
+        case "divide":
+            $(".calculation").html("/");
+            break;
+    }
+})
+
+//  <-- select second number -->
+
+function secondNum() {
+    $(".number").on("click", () => {
+        var e = this.button;
+        switch(e){
+            case "number-one":
+                num2 = $(".number-one").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numbertwo":
+                num2 = $("#numbertwo").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numberthree":
+                num2 = $("#numberthree").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numberfour":
+                num = $("#numberfour").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numberfive":
+                num2 = $("#numberfive").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numbersix":
+                num2 = $("#numbersix").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numberseven":
+                num2 = $("#numberseven").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numbereight":
+                num2 = $("#numbereight").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numbernine":
+                num2 = $("#numbernine").text();
+                $(".secondNumber").html(num2);
+                break;
+            case "numberzero":
+                num2 = $("#numberzero").text();
+                $(".secondNumber").html(num2);
+                break;
+        }
+    });
+}
+
+//  <-- equal button  -->
+
+$("button").on("click", (event) => {
+    var e = event.target.id;
+    switch(e) {
+        case "equal":
+            $(".result").html("=");
+            break;
+    }
 });
 
-document.querySelector("#times").addEventListener("click", 
-    function mult (num1, num2) {
-        return num1 * num2;
-});
+//  <-- math symbol functions -->
 
-document.querySelector("#divide").addEventListener("click", 
-    function div (num1, num2) {
-        return num1 / num2;
-});
+function add (num1, num2) {
+    return num1 + num2;     
+}
 
-document.querySelector("#equal").addEventListener("click", 
-    function calculate (num1, num2, operator) {
-        return operator (num1, num2);
-});
+function sub (num1, num2) {
+    return num1 - num2;
+}
+
+function mult (num1, num2) {
+    return num1 * num2;
+}
+
+function div (num1, num2) {
+    return num1 / num2;
+}
+
+function calculate (num1, num2, operator) {
+    return operator (num1, num2);
+}
